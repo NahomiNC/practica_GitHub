a4f719567528cbf54e5cd9fcc1408d8d35ec513b
dam117 <>
2017-05-21 13:47:56 +0200

    1- Creación del proyecto

---
+++ src/paquete/Main.java
@@ -0,0 +1,19 @@
+/*
+ * Práctica GitHub
+ */
+package paquete;
+
+/**
+ *
+ * @author dam117
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
