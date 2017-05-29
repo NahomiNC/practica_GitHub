349e368df4b484b19fed2ff8c33bfa06953cac22
Alebt92 <Alejandrobt_92@hotmail.es>
2017-05-29 21:04:24 +0200

    Commit colaborador
    prueba de colaborador

--- src/paquete/Main.java
+++ src/paquete/Main.java
@@ -15,6 +15,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Subido desde la pág de GitHub");
         System.out.println("Edición desde la rama");
+        System.out.println("Hola, soy un colaborador");
     }
     
 }
